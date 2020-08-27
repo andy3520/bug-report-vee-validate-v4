b96e67284248fb717d23f7772a58b2eaa6e32099
An Nguyen Hieu Duc <andy.annguyen@gmail.com>
2020-08-27 23:21:08 +0700

    vee-validate v4 bug example

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,36 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+import "@/assets/styles/main.css";
 import router from "./router";
 import store from "./store";
+import { localize } from "@vee-validate/i18n";
+import en from "@vee-validate/i18n/dist/locale/en.json";
+import * as rules from "@vee-validate/rules";
+import { configure, defineRule } from "vee-validate";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+configure({
+  generateMessage: localize({ en }),
+});
+
+type ValidationRuleFunction = (
+  value: any,
+  params: any[] | Record<string, any>,
+  ctx: FieldContext
+) => boolean | string | Promise<boolean | string>;
+
+interface FieldContext {
+  field: string;
+  value: any;
+  form: Record<string, any>;
+  rule: {
+    name: string;
+    params?: Record<string, any> | any[];
+  };
+}
+
+Object.keys(rules).forEach((rule: string) => {
+  defineRule(rule, (rules as Record<string, ValidationRuleFunction>)[rule]);
+});
+
+createApp(App).use(store).use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container mx-auto">
+    <h1 class="text-xl">Vee validate v4 bug with built-in rules</h1>
+    <p class="p-4 italic font-bold text-red-500 bg-indigo-200">
+      Description: The placeholder for name {_field_} or param like {length} is
+      not replacing by real value
+    </p>
+    <div class="w-full p-4 bg-green-200">
+      Field
+      <v-field
+        name="text"
+        v-slot="{ field, errorMessage }"
+        rules="required|min:3"
+      >
+        <input
+          placeholder="required|min:3"
+          v-bind="field"
+          type="text"
+          class="p-2"
+        />
+        <div class="py-2 font-bold text-red-500">Error: {{ errorMessage }}</div>
+      </v-field>
+    </div>
+
+    <div class="w-full p-4 bg-blue-200">
+      useField
+      <input
+        v-model="validateValue"
+        class="p-2"
+        placeholder="required|min:3"
+        @input="validationTracking"
+      />
+      <div class="py-2 font-bold text-red-500">Error: {{ errorMessage }}</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, ref } from "vue";
+import { Field, useField } from "vee-validate";
 
 export default defineComponent({
-  name: "Home",
   components: {
-    HelloWorld
-  }
+    "v-field": Field,
+  },
+  name: "Home",
+  setup() {
+    const inputTest = ref("");
+
+    const {
+      value: validateValue,
+      errorMessage,
+      handleChange: validationTracking,
+    } = useField("test", "required|min:3", {
+      initialValue: inputTest,
+    });
+
+    return {
+      inputTest,
+      validationTracking,
+      errorMessage,
+      validateValue,
+    };
+  },
 });
 </script>
